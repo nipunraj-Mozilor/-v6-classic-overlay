ca151d5dbfc525c29832e362fed0bbc0441ccea8
nipunraj <nipunraj99@gmail.com>
2023-03-19 23:58:03 +0530

    console correction

--- public/scirpt.js
+++ public/scirpt.js
@@ -2,6 +2,7 @@ const buttons = ['cky-btn-accept', 'cky-btn-reject'];
 
 let breakShowupWait = false;
 window.addEventListener('load', function () {
+  console.log('in load');
   waitForElement('cky-consent-container', false, handleConsentBannerShown);
 });
 function waitForElement(selector, isShowup, callback) {
@@ -23,9 +24,9 @@ function handleConsentBannerShown(element) {
   overlayElement.setAttribute('class', 'cky-overlay');
   element[0].parentNode.insertBefore(overlayElement, element.nextSibling);
   for (let i = 0; i < buttons.length; i++) {
-    console.log('buttons', buttons[i]);
+    console.log('buttons in loop', buttons[i]);
     const buttonElement = document.querySelector(`.${buttons[i]}`);
-    console.log(buttonElement);
+    console.log('cliked button', buttonElement);
     buttonElement &&
       buttonElement.addEventListener('click', removeOverlay(overlayElement));
   }
