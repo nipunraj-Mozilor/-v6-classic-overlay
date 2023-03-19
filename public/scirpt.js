67dd3b656cd5d41eb613429f3631c662fbc6f055
nipunraj <nipunraj99@gmail.com>
2023-03-19 15:00:53 +0530

    test 3

--- public/scirpt.js
+++ public/scirpt.js
@@ -1,9 +1,9 @@
-const selectEle = document.getElementsByTagName('h2')
-selectEle[0].style.color = 'red'
+// const selectEle = document.getElementsByTagName('h2')
+// selectEle[0].style.color = 'red'
 
 const buttons = ['cky-btn-accept', 'cky-btn-reject', 'cky-btn-close']
 
-let breakPopupWait = true
+let breakPopupWait = false
 window.addEventListener('load', function () {
   waitForElement('cky-consent-container', false, handleConsentBannerShown)
 })
@@ -29,18 +29,18 @@ function handleConsentBannerShown(element) {
     buttonElement &&
       buttonElement.addEventListener('click', removeOverlay(overlayElement))
   }
-  waitForElement('cky-settings-popup', true, function (element) {
-    element &&
-      document
-        .getElementById('cky-btn-custom-accept')
-        .addEventListener('click', removeOverlay(overlayElement))
-  })
-  waitForElement('cky-ccpa-settings-popup', true, function (element) {
-    element &&
-      document
-        .getElementById('cky-btn-confirm')
-        .addEventListener('click', removeOverlay(overlayElement))
-  })
+  // waitForElement('cky-settings-popup', true, function (element) {
+  //   element &&
+  //     document
+  //       .getElementById('cky-btn-custom-accept')
+  //       .addEventListener('click', removeOverlay(overlayElement))
+  // })
+  // waitForElement('cky-ccpa-settings-popup', true, function (element) {
+  //   element &&
+  //     document
+  //       .getElementById('cky-btn-confirm')
+  //       .addEventListener('click', removeOverlay(overlayElement))
+  // })
 }
 
 function removeOverlay(element) {
