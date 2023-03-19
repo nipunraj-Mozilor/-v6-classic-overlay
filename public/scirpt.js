f7b7eec8368330326d0193dba4ba65fcece8db57
nipunraj <nipunraj99@gmail.com>
2023-03-19 16:22:57 +0530

    test 10

--- public/scirpt.js
+++ public/scirpt.js
@@ -1,10 +1,6 @@
 const selectEle = document.getElementsByTagName('h2')
 selectEle[0].style.color = 'red'
 
-window.addEventListener('load', () => {
-  document.body.classList.remove('cky-overlay')
-})
-
 if (
   !document.querySelector(
     '.cky-consent-container .cky-classic-bottom .cky-hide'
