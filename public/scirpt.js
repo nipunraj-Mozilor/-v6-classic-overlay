cace70cb0751ff79798b23c0862ade271b79b0d1
nipunraj <nipunraj99@gmail.com>
2023-03-19 15:24:28 +0530

    test 5

--- public/scirpt.js
+++ public/scirpt.js
@@ -1,54 +1,19 @@
-// const selectEle = document.getElementsByTagName('h2')
-// selectEle[0].style.color = 'red'
+const selectEle = document.getElementsByTagName('h2')
+selectEle[0].style.color = 'red'
 
-const buttons = ['cky-btn-accept', 'cky-btn-reject']
-
-let breakPopupWait = false
-window.addEventListener('load', function () {
-  waitForElement('cky-consent-container', false, handleConsentBannerShown)
-})
-
-function waitForElement(selector, isPopup, callback) {
-  if (isPopup && breakPopupWait) {
-    breakPopupWait = false
-    return callback()
-  }
-  const element = document.getElementById(selector)
-  if (element && element.style.display !== 'none') return callback(element)
-  setTimeout(function () {
-    waitForElement(selector, isPopup, callback)
-  }, 200)
-}
-
-function handleConsentBannerShown(element) {
-  const overlayElement = document.createElement('div')
+if (
+  !document.querySelector('.cky-consent-container.cky-classic-bottom.cky-hide')
+) {
   document.body.classList.add('cky-overlay')
-  // overlayElement.setAttribute('class', 'cky-overlay')
-  // element.parentNode.insertBefore(overlayElement, element.nextSibling)
-  for (let i = 0; i < buttons.length; i++) {
-    const buttonElement = document.getElementById(buttons[i])
-    buttonElement &&
-      buttonElement.addEventListener('click', removeOverlay(overlayElement))
-  }
-  // waitForElement('cky-settings-popup', true, function (element) {
-  //   element &&
-  //     document
-  //       .getElementById('cky-btn-custom-accept')
-  //       .addEventListener('click', removeOverlay(overlayElement))
-  // })
-  // waitForElement('cky-ccpa-settings-popup', true, function (element) {
-  //   element &&
-  //     document
-  //       .getElementById('cky-btn-confirm')
-  //       .addEventListener('click', removeOverlay(overlayElement))
-  // })
 }
 
-function removeOverlay(element) {
-  return function () {
+jQuery(document).on(
+  'click',
+  '.cky-btn-reject,.cky-btn-preferences,.cky-btn-accept',
+  function () {
     document.body.classList.remove('cky-overlay')
-    // element.parentNode.removeChild(element)
-    breakPopupWait = true
-    waitForElement('cky-consent', false, handleConsentBannerShown)
   }
-}
+)
+jQuery(document).on('click', '.cky-btn-revisit', function () {
+  document.body.classList.add('cky-overlay')
+})
