b0e7c790e36c466f4e9cd6e2f833d6adc2b07e4f
nipunraj <nipunraj99@gmail.com>
2023-03-20 13:03:52 +0530

    new 1

--- public/scirpt.js
+++ public/scirpt.js
@@ -1,9 +1,9 @@
 const buttons = ['cky-btn-accept', 'cky-btn-reject'];
 
 let breakShowupWait = false;
-window.addEventListener('load', function () {
-  waitForElement('cky-consent-container', false, handleConsentBannerShown);
-});
+// window.addEventListener('load', function () {
+// });
+waitForElement('cky-consent-container', false, handleConsentBannerShown);
 function waitForElement(selector, isShowup, callback) {
   if (isShowup && breakShowupWait) {
     breakShowupWait = false;
@@ -29,7 +29,12 @@ function waitForElement(selector, isShowup, callback) {
 function handleConsentBannerShown(element) {
   console.log('inside callback element', element);
   const overlayElement = document.createElement('div');
-  overlayElement.setAttribute('class', 'cky-overlay');
+  overlayElement.setAttribute('style', 'cky-overlay');
+  overlayElement.setAttribute(
+    'style',
+    'position:fixed;top:0;left:0;width:100%;height:100%;background-color:rgba(0, 0, 0, 0.5);z-index:9999;'
+  );
+
   element.parentNode.insertBefore(overlayElement, element.nextSibling);
   for (let i = 0; i < buttons.length; i++) {
     // console.log('buttons in loop', buttons[i]);
