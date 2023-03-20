4358c3eb59c9bff3a7f080e2329c747ac25a5870
nipunraj <nipunraj99@gmail.com>
2023-03-20 11:33:54 +0530

    reeha code 8 console

--- public/scirpt.js
+++ public/scirpt.js
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const elements = document.querySelectorAll(
     '#cky-btn-reject, #cky-btn-preferences, #cky-btn-accept'
   );
+  console.log(elements);
   elements.forEach((element) => {
     element.addEventListener('click', () => {
       document.body.classList.remove('cky-overlay');
