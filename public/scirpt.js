f5252da90dd6485df2a9018ff6a1d351cd8928a0
nipunraj <nipunraj99@gmail.com>
2023-03-20 10:54:24 +0530

    reeha code

--- public/scirpt.js
+++ public/scirpt.js
@@ -1,49 +1,34 @@
-const buttons = ['cky-btn-accept', 'cky-btn-reject'];
-
-let breakShowupWait = false;
 window.addEventListener('load', function () {
-  waitForElement('cky-consent-container', false, handleConsentBannerShown);
+  setTimeout(function () {
+    if (
+      !document.querySelector(
+        '.cky-consent-container.cky-classic-bottom.cky-hide'
+      )
+    ) {
+      document.body.classList.add('cky-overlay');
+    }
+  }, 2000);
 });
-function waitForElement(selector, isShowup, callback) {
-  if (isShowup && breakShowupWait) {
-    breakShowupWait = false;
-    return callback();
-  }
-  const element = document.querySelector(`.${selector}`);
-  // console.log('element2', element2);
-  if (element && element.style.display !== 'none') {
-    console.log('is none');
-    return callback(element);
-  }
-  // if (element && element.classList.contains('cky-hide')) {
-  //   const computedStyle = window.getComputedStyle(element);
-  //   if (computedStyle.display === 'block') {
-  //     document.body.classList.add('cky-overlay');
-  //   }
-  // }
-  setTimeout(() => {
-    waitForElement(selector, isShowup, callback);
-  }, 200);
-}
 
-function handleConsentBannerShown(element) {
-  console.log('inside callback element', element);
-  const overlayElement = document.createElement('div');
-  overlayElement.setAttribute('class', 'cky-overlay');
-  element.parentNode.insertBefore(overlayElement, element.nextSibling);
-  for (let i = 0; i < buttons.length; i++) {
-    // console.log('buttons in loop', buttons[i]);
-    const buttonElement = document.querySelector(`.${buttons[i]}`);
-    // console.log('cliked button', buttonElement);
-    buttonElement &&
-      buttonElement.addEventListener('click', removeOverlay(overlayElement));
-  }
-}
+const element = document.querySelector(
+  '.cky-btn-reject,.cky-btn-preferences,.cky-btn-accept'
+);
+element.addEventListener('click', () => {
+  document.body.classList.remove('cky-overlay');
+});
 
-function removeOverlay(element) {
-  return () => {
-    element.parentNode.removeChild(element);
-    breakPopupWait = true;
-    waitForElement('cky-consent', false, handleConsentBannerShown);
-  };
-}
+// jQuery(document).on(
+//   'click',
+//   '.cky-btn-reject,.cky-btn-preferences,.cky-btn-accept',
+//   function () {
+//     document.body.classList.remove('cky-overlay');
+//   }
+// );
+const element2 = document.querySelector('.cky-btn-revisit');
+element2.addEventListener('click', () => {
+  document.body.classList.add('cky-overlay');
+});
+
+// jQuery(document).on('click', '.cky-btn-revisit', function () {
+//   document.body.classList.add('cky-overlay');
+// });
