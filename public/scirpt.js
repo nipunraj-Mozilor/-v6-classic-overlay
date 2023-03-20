549947024d09f1d49b97060467d5fddfec86038d
nipunraj <nipunraj99@gmail.com>
2023-03-20 11:41:25 +0530

    reeha code 10 window

--- public/scirpt.js
+++ public/scirpt.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
   setTimeout(function () {
     if (
-      !document.querySelector(
+      !window.querySelector(
         '.cky-consent-container.cky-classic-bottom.cky-hide'
       )
     ) {
@@ -11,12 +11,6 @@ window.addEventListener('load', function () {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const element = document.querySelector(
-  //   '.cky-btn-reject,.cky-btn-preferences,.cky-btn-accept'
-  // );
-  // element.addEventListener('click', () => {
-  //   document.body.classList.remove('cky-overlay');
-  // });
   const elements = document.querySelectorAll(
     '#cky-btn-reject, #cky-btn-preferences, #cky-btn-accept'
   );
@@ -28,35 +22,9 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// const element = document.querySelector(
-//   '.cky-btn-reject,.cky-btn-preferences,.cky-btn-accept'
-// );
-// element.addEventListener('click', () => {
-//   document.body.classList.remove('cky-overlay');
-// });
-
-// jQuery(document).on(
-//   'click',
-//   '.cky-btn-reject,.cky-btn-preferences,.cky-btn-accept',
-//   function () {
-//     document.body.classList.remove('cky-overlay');
-//   }
-// );
-// const element2 = document.querySelector('.cky-btn-revisit');
-// element2.addEventListener('click', () => {
-//   document.body.classList.add('cky-overlay');
-// });
 document.addEventListener('DOMContentLoaded', () => {
   const element2 = document.querySelector('.cky-btn-revisit-wrapper');
-  // console.log('element 2', element2);
-  // element2.addEventListener('click', () => {
-  //   document.body.classList.add('cky-overlay');
-  // });
   if (element2 && element2.classList.contains('cky-btn-revisit')) {
     document.body.classList.add('cky-overlay');
   }
 });
-
-// jQuery(document).on('click', '.cky-btn-revisit', function () {
-//   document.body.classList.add('cky-overlay');
-// });
